ba884c416c0b78c4231af6e4c499a4550b12c800
johtela <tommi.johtela@dnainternet.net>
2018-08-29 16:57:11 +0300

    Intermediate commit.

--- ComposeFX.Compiler.Tests/UnitTest1.cs
+++ ComposeFX.Compiler.Tests/UnitTest1.cs
@@ -1,6 +1,20 @@
 namespace ComposeFX.Compiler.Tests
 {
+	using ComposeFX.Maths;
 	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using System.Runtime.InteropServices;
+
+	[StructLayout (LayoutKind.Sequential, Pack = 4)]
+	public struct PositionalVertex
+	{
+		public Vec3 position;
+		public Vec3 normal;
+
+		public override string ToString ()
+		{
+			return string.Format ("[Vertex: Position={0}, Normal={1}]", position, normal);
+		}
+	}
 
 	[TestClass]
     public class UnitTest1
